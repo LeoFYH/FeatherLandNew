71ff9e3be5aac2c160b9184f87b03b5f35c8cace
13520524204 <1181341934@qq.com>
2025-08-23 08:59:20 +0800

    更新媒体按钮动画

---
+++ Assets/Scripts/New/ViewController/UI/ButtonAnimSp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BirdGame
+{
+    [RequireComponent(typeof(Image))]
+    public class ButtonAnimSp : ViewControllerBase
+    {
+        public Sprite normal;
+        public Sprite higLight;
+
+        private Image thisImage;
+        
+        private void Start()
+        {
+            thisImage = GetComponent<Image>();
+            OnNormal();
+        }
+
+        public void OnNormal()
+        {
+            thisImage.sprite = normal;
+            GetComponent<RectTransform>().sizeDelta = normal.rect.size;
+        }
+
+        public void OnHigLight()
+        {
+            thisImage.sprite = higLight;
+            GetComponent<RectTransform>().sizeDelta = higLight.rect.size;
+        }
+    }
+}
