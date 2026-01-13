e6752ab33d28e217b277187f5c37f0ae1b632100
LeoFu11111 <fuleo2400@gmail.com>
2026-01-13 12:15:05 +0800

    3

--- Assets/Scripts/New/SliderClickHandler.cs
+++ Assets/Scripts/New/SliderClickHandler.cs
@@ -7,10 +7,8 @@ using TMPro;
 public class SliderBarClickHandler : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
 {
     public Slider slider;
-    public float dragSensitivity = 1.0f;
     
     private bool isDragging = false;
-    private Vector2 previousMousePos;
 
     private void Start()
     {
@@ -22,7 +20,8 @@ public class SliderBarClickHandler : MonoBehaviour, IPointerClickHandler, IPoint
     {
         if (isDragging)
         {
-            HandleSmartDrag();
+            // Directly set slider value to mouse position while dragging
+            SetValueFromMousePosition(Input.mousePosition);
         }
     }
 
@@ -30,14 +29,15 @@ public class SliderBarClickHandler : MonoBehaviour, IPointerClickHandler, IPoint
     {
         if (!isDragging)
         {
-            SetValueFromClick(eventData.position);
+            SetValueFromMousePosition(eventData.position);
         }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         isDragging = true;
-        previousMousePos = eventData.position;
+        // Immediately set value to where user clicked
+        SetValueFromMousePosition(eventData.position);
     }
 
     public void OnPointerUp(PointerEventData eventData)
@@ -45,56 +45,7 @@ public class SliderBarClickHandler : MonoBehaviour, IPointerClickHandler, IPoint
         isDragging = false;
     }
 
-    private void HandleSmartDrag()
-    {
-        Vector2 currentMousePos = Input.mousePosition;
-        Vector2 delta = currentMousePos - previousMousePos;
-
-        // Only process if there's meaningful movement
-        if (delta.magnitude > 0.1f)
-        {
-            // Get the relevant axis based on slider direction
-            float relevantDelta = GetRelevantAxisDelta(delta);
-            
-            // Apply drag
-            float valueChange = (relevantDelta / GetScreenSize()) * dragSensitivity;
-            float valueRange = slider.maxValue - slider.minValue;
-            
-            slider.value += valueChange * valueRange;
-            slider.value = Mathf.Clamp(slider.value, slider.minValue, slider.maxValue);
-
-            previousMousePos = currentMousePos;
-        }
-    }
-
-    private float GetRelevantAxisDelta(Vector2 delta)
-    {
-        // For horizontal sliders, only use X movement
-        if (slider.direction == Slider.Direction.LeftToRight || slider.direction == Slider.Direction.RightToLeft)
-        {
-            return delta.x;
-        }
-        // For vertical sliders, only use Y movement
-        else
-        {
-            return delta.y;
-        }
-    }
-
-    private float GetScreenSize()
-    {
-        // Use appropriate screen dimension based on slider direction
-        if (slider.direction == Slider.Direction.LeftToRight || slider.direction == Slider.Direction.RightToLeft)
-        {
-            return Screen.width;
-        }
-        else
-        {
-            return Screen.height;
-        }
-    }
-
-    private void SetValueFromClick(Vector2 screenPosition)
+    private void SetValueFromMousePosition(Vector2 screenPosition)
     {
         RectTransform rect = GetComponent<RectTransform>();
         Vector2 localPoint;
