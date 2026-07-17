9e9d4a1cf19014a75078473e4c766c88739a8a52
13520524204 <1181341934@qq.com>
2026-07-18 00:35:48 +0800

    更新森林灯夜晚动画

--- Assets/Scripts/New/ViewController/Game/ResetSpriteController.cs
+++ Assets/Scripts/New/ViewController/Game/ResetSpriteController.cs
@@ -1,14 +1,19 @@
-﻿using UnityEngine;
+﻿using QFramework;
+using UnityEngine;
 
 namespace BirdGame
 {
     public class ResetSpriteController : ViewControllerBase
     {
         public Sprite[] sps;
+        public GameObject[] anims;
         public SpriteRenderer sr;
+
+        private int id;
 
         public void SetSp(int index)
         {
+            id = index;
             if (sps == null || sps.Length == 0)
                 return;
             if (sps.Length <= index)
@@ -18,6 +23,23 @@ namespace BirdGame
             }
 
             sr.sprite = sps[index];
+        }
+
+        private void Start()
+        {
+            this.RegisterEvent<SwitchWeatherEvent>(evt =>
+            {
+                if(evt.index == 2)
+                {
+                    sr.enabled = false;
+                    anims[id].SetActive(true);
+                }
+                else
+                {
+                    sr.enabled = true;
+                    anims[id].SetActive(false);
+                }
+            }).UnRegisterWhenGameObjectDestroyed(gameObject);
         }
     }
 }
